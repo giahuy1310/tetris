2afa51f77543cc59d2b97d06c54942af57253d41
cryborg1211 <caokhuong12311@gmail.com>
2024-01-13 14:18:59 +0700

    Update vertical move

--- Shape.java
+++ Shape.java
@@ -1,10 +1,11 @@
 import java.awt.*;
+
 public class Shape {
     public static final int BOARD_WIDTH = 10;
     public static final int BOARD_HEIGHT = 20;
     public static final int BLOCK_SIZE = 30;
 
-    private int x= 0, y =0;
+    private int x = 0, y = 0;
     private int normal = 600;
     private int fast = 100;
     private int delayTimeForMovement = normal;
@@ -13,33 +14,36 @@ public class Shape {
     private int deltaX = 0;
     private boolean collision = false;
 
-    private int [] [] coords;
+    private int[][] coords;
     private GameArea board;
     public Color color;
-    public Shape(int[][] coords, GameArea board, Color color){
+
+    public Shape(int[][] coords, GameArea board, Color color) {
         this.coords = coords;
         this.board = board;
         this.color = color;
     }
 
-    public void setX(int x){
+    public void setX(int x) {
         this.x = x;
     }
-    public void setY(int y){
+
+    public void setY(int y) {
         this.y = y;
     }
-    public void reset(){
+
+    public void reset() {
         this.x = 4;
         this.y = 0;
         collision = false;
     }
 
-    public void update(){
-        if (collision){
+    public void update() {
+        if (collision) {
             // fill color for the board
-            for ( int row = 0; row < coords.length; row++){
-                for (int col = 0; col < coords[0].length; col++){
-                    if (coords[row][col] !=0){
+            for (int row = 0; row < coords.length; row++) {
+                for (int col = 0; col < coords[0].length; col++) {
+                    if (coords[row][col] != 0) {
                         board.getBoard()[y + row][x + col] = color;
                     }
                 }
@@ -48,51 +52,63 @@ public class Shape {
             board.setCurrentShape();
             return;
         }
-        // check movent horizontal
-        if (!(x + deltaX + coords[0].length > BOARD_WIDTH) && !(x + deltaX < 0)){
-            x+= deltaX;
+        // horizontal movement check
+        if (!(x + deltaX + coords[0].length > BOARD_WIDTH) && !(x + deltaX < 0)) {
+            x += deltaX;
         }
         deltaX = 0;
-        if (System.currentTimeMillis() - beginTime > delayTimeForMovement){
-            // vertical movement
-            if (!(y + 1 + coords.length > BOARD_HEIGHT)){
-                for (int row = 0 ; row < coords.length; row++){
-                    for (int col = 0; col < coords[row].length; col++){
-                        
+
+        // vertical movement check
+        if (System.currentTimeMillis() - beginTime > delayTimeForMovement) {
+            if (!(y + 1 + coords.length > BOARD_HEIGHT)) {
+                for (int row = 0; row < coords.length; row++) {
+                    for (int col = 0; col < coords[row].length; col++) {
+                        if (coords[row][col] != 0) {
+                            if (board.getBoard()[y + 1 + row][x + deltaX + col] != null) {
+                                collision = true;
+                            }
+                        }
                     }
                 }
-                y++;
-            } 
-            else {
+                if (!collision) {
+                    y++;
+                }
+            } else {
                 collision = true;
             }
-        
+
             beginTime = System.currentTimeMillis();
         }
     }
-    public void render(Graphics g){
-        //draw the shape
-        for (int row = 0; row< coords.length; row++){
-            for (int col  = 0; col<coords[0].length; col++){
-                if (coords[row][col]!=0){
-                g.setColor(Color.RED);
-                g.fillRect(col * BLOCK_SIZE + x * BLOCK_SIZE , row * BLOCK_SIZE + y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
 
+    public void render(Graphics g) {
+        // draw the shape
+        for (int row = 0; row < coords.length; row++) {
+            for (int col = 0; col < coords[0].length; col++) {
+                if (coords[row][col] != 0) {
+                    g.setColor(Color.RED);
+                    g.fillRect(col * BLOCK_SIZE + x * BLOCK_SIZE, row * BLOCK_SIZE + y * BLOCK_SIZE, BLOCK_SIZE,
+                            BLOCK_SIZE);
+
+                }
+            }
         }
+        //
     }
-}
-    // 
-    }
-    public void speedUp(){
+
+    public void speedUp() {
         delayTimeForMovement = fast;
     }
-    public void speedDown(){
+
+    public void speedDown() {
         delayTimeForMovement = normal;
     }
-    public void moveLeft(){
+
+    public void moveLeft() {
         deltaX = -1;
     }
-    public void moveRight(){
+
+    public void moveRight() {
         deltaX = 1;
     }
 }
